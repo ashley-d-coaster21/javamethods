d167b0f1d1e07e451ecb0e249623453b16c41e57
ashley-d-coaster21 <107349738+ashley-d-coaster21@users.noreply.github.com>
2023-11-28 11:30:51 +0530

    Add files via upload

---
+++ oops1.py
@@ -0,0 +1,32 @@
+class Student:
+    def setData(self,rollno,name,corse,marks);
+    self.rollno = rollno
+    self.name=name
+    self.course=course
+    self.marks=marks
+
+    def displayStudent(self):
+        print("roll number",self.rollno)
+        print("name",self.name)
+        print("course",self.course)
+        print("marks",self.mark)
+
+        def calculatedgGrade(self)
+            if(self.marks>480)
+            self.grade="Distintion"
+            elif self.mark>360:
+            self.grade="firstclass"
+            elif self.mark>240:
+            self.grade="second class"
+            elif 
+            self.grade="failled"
+            r=int(input)"enter roll"))
+            n=intput("enter your name")
+            c=input("enter course")
+            m=int(input("enter marks"))
+            print("result")self.print)
+            s1=student()
+            s1=s1.setdata(r,n,c,m)
+            s1.displaystudent()
+            s1.calculateGrade()
+            
